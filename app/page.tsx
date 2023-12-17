a11552031f77c2617dc3714e7cc7fdb5a280231d
Erwin <erwinshrestha17@gmail.com>
2023-12-17 13:16:54 +0545

    Image galary

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,122 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <main className={'bg-gray-100 min-h-screen w-screen'}>
+        <section className={'bg-white max-w-screen-2xl m-auto '}>
+          {/*Nav Bar*/}
+          <nav className={'bg-white p-2 flex justify-between'}>
+            <a href={'#'} className={'font-bold text-gray-700 text-2xl'}>Open Table</a>
+            <div>
+              <div className={'flex'}>
+                <button className={'bg-blue-400 text-white border p-1 px-4 rounded mr-3'}>Sign In</button>
+                <button className={' border p-1 px-4 rounded'}>Sign Out</button>
+              </div>
+            </div>
+          </nav>
+          {/*Nav Bar*/}
+        </section>
+          {/*Header*/}
+        <section className={'h-96 overflow-hidden'}>
+          <div className={'bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center'}>
+            <h1 className={'text-7xl text-white capitalize text-shadow text-center'}>Milestone Grills</h1>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          </div>
+        </section>
+          {/*Header*/}
+          {/*Descriptions*/}
+        <section className={'flex m-auto w-2/3 justify-center items-start 0 -mt-10 '}>
+          <div className={'bg-white w-[70%] rounded p-3 shadow'}>
+              {/*Reasturant Nav Bar*/}
+              <nav className={'flex text-reg border-b pb-2'}>
+                <a href={'#'} className={'mr-7'}>Overviews</a>
+                <a href={'#'} className={'mr-7'}>Menus</a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              </nav>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+              {/*Reasturant Nav Bar*/}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              {/*Title*/}
+              <div className={'mt-4 border-b pb-6'}>
+                  <h1 className={'font-bold text-6xl'}>Milestone Grill</h1>
+              </div>
+              {/*Title*/}
+              {/*Ratings*/}
+              <div className={'flex items-end'}>
+                  <div className={'ratings mt-2 flex items-center'}>
+                      <p>*****</p>
+                      <p className={"text-reg ml-3"}>4.8</p>
+                  </div>
+                  <div>
+                      <p className={'text-reg ml-4'}>600</p>
+                  </div>
+
+              </div>
+              {/*Ratings*/}
+              {/*Descriptions*/}
+              <div className={'mt-4'}>
+                  <p className={'text-lg font-light'}>
+                      The classics ypu love prepared with a perfect twist,
+                      all served up in an atmosphere that feels right.
+                      Thats the Milestones promise.So, whether your celebrating a milestone,
+                      making the most of Happy Hour or enjoy brunch with friends ,
+                      you can be sure that every Milestones experience is a simple and perfectly memorable one.
+                  </p>
+              </div>
+              {/*Descriptions*/}
+              {/*Images*/}
+              <div>
+                  <h1 className={'font-bold text-3xl mt-10 mb-7 border-b-5'}>5 image</h1>
+                  <div className={'flex flex-wrap'}>
+                      <img className={'w-55 h-44 mr-1 mb-1'}
+                           src={'https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpeg'}/>
+                      <img className={'w-55 h-44 mr-1 mb-1'}
+                           src={'https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpeg'}/>
+                      <img className={'w-55 h-44 mr-1 mb-1'}
+                           src={'https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpeg'}/>
+                      <img className={'w-55 h-44 mr-1 mb-1'}
+                           src={'https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpeg'}/>
+                      <img className={'w-55 h-44 mr-1 mb-1'}
+                           src={'https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpeg'}/>
+
+                  </div>
+              </div>
+              {/*Images*/}
+
+              {/*Reviews*/}
+              <div>
+                  <h1 className={'font-bold text-3xl mt-10 mb-7 border-b pb-5 '}>What 100 people are saying</h1>
+                  {/*Reviews Card*/}
+                  <div className="border-b pb-7 mb-7">
+                      <div className="flex">
+                          <div className="w-1/6 flex flex-col items-center">
+                              <div className="rounded-full bg-blue-400 w-15 h-15 items-center justify-center">
+                                  <h2 className={'text-white text-2xl text-center'}>MJ</h2>
+                              </div>
+                              <p className={'font-bold text-center'}>Michale Jordan</p>
+                          </div>
+                          <div className={'ml-10 w-5/6'}>
+                              <div className={'flex items-center'}>
+                                  <div className="flex mr-5">
+                                      *****
+                                  </div>
+                              </div>
+                              <div className={'mt-5'}>
+                                  <p className={"text-lg font-light"}>i had a good time</p>
+                              </div>
+
+                          </div>
+                      </div>
+                  </div>
+                  {/*Reviews Card*/}
+              </div>
+              {/*Reviews*/}
+
+          </div>
+
+        </section>
+          {/*Descriptions*/}
+
+      </main>
   )
 }
